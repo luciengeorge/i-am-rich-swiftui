c9125ac72c6ff5101767d1a276a2d6f8cb87f9fd
Lucien George <lucien.george@shopify.com>
2023-02-07 17:38:23 +0000

    swiftui i am rich app

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Lucien George on 03/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro Max"))
+    }
+}
